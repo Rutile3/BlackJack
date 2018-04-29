02008b3111e9a7c5281b167582c3454b3a40ebf6
ルチルさん <29337073+Rutile3@users.noreply.github.com>
2018-04-29 23:02:33 +0900

    カードの初期配布部分を作成

--- main.py
+++ main.py
@@ -6,23 +6,29 @@ def make_deck():
     suits = ['S', 'H', 'D', 'C']
     ranks = range(1, 13+1)
     deck = [(x, y) for x in ranks for y in suits]
-    print(deck)
     random.shuffle(deck)
     return deck
 
 def main():
-    print(make_deck())
-    player_money = 0 #あと書き直す
+    player_money = 100 #あと書き直す
     while player_money > 0:
-        pass
-        #変数の初期化
+        player_hand = []
+        dealer_hand = []
+        deck = make_deck()
+        player_hand.append(deck.pop())
+        player_hand.append(deck.pop())
+        dealer_hand.append(deck.pop())
+        dealer_hand.append(deck.pop())
+        print(player_hand)
+        print(dealer_hand)
         #ベット額の選択
         #お互いにカードを二枚づつ引く
         #プレーヤーのターン
         #ディーラーのターン
         #手札公開
         #勝敗判定
+        break # でバック用のループ抜け出し
     #gameover
 
-if __name__ == '__main__':
+if __name__ == '__main__': #直接実行されたときのみ実行
     main()
